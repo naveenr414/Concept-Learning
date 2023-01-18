4a41dd0a76043daa7b4ae1b06513ef82f314405e
Naveen Raman <njr61@cl.cam.ac.uk>
2023-01-18 09:57:15 +0000

    Created Evaluate Hierarchies notebook and fixed test for metrics

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -3,30 +3,41 @@ from src.hierarchy import *
 from src.concept_vectors import *
 
 def test_stability():
-    mnist_attributes = ['{}_color'.format(i) for i in range(5)] + ['{}_number'.format(i) for i in range(5)]
+    mnist_attributes = get_mnist_attributes()
     
-    assert stability_metric(create_linkage_hierarchy,load_cem_vectors_simple,'mnist',mnist_attributes,[45,45]) == 0
-    assert stability_metric(create_linkage_hierarchy,load_tcav_vectors_simple,'mnist',mnist_attributes,[45,45]) == 0
-    assert stability_metric(create_linkage_hierarchy,load_tcav_vectors_simple,'mnist',mnist_attributes,[43,44,45])>0
-    assert stability_metric(create_linkage_hierarchy,load_cem_vectors_simple,'mnist',mnist_attributes,[43,44,45])>0
+    assert stability_metric(create_linkage_hierarchy,
+                            load_cem_vectors_simple,'mnist',mnist_attributes,[45,45]) == 0
+    assert stability_metric(create_linkage_hierarchy,
+                            load_tcav_vectors_simple,'mnist',mnist_attributes,[45,45]) == 0
+    assert stability_metric(create_linkage_hierarchy,
+                            load_tcav_vectors_simple,'mnist',mnist_attributes,[43,44,45])>0
+    assert stability_metric(create_linkage_hierarchy,
+                            load_cem_vectors_simple,'mnist',mnist_attributes,[43,44,45])>0
 
 def test_image_robustness():
-    mnist_attributes = ['{}_color'.format(i) for i in range(5)] + ['{}_number'.format(i) for i in range(5)]
+    mnist_attributes = get_mnist_attributes()
     
-    assert robustness_image_metric(create_linkage_hierarchy,load_tcav_vectors_simple,'mnist',mnist_attributes,[43,44,45]) > 0
-    assert robustness_image_metric(create_linkage_hierarchy,load_tcav_vectors_simple,'mnist',mnist_attributes,[43]) > 0
-    assert robustness_image_metric(create_linkage_hierarchy,load_tcav_vectors_simple,'mnist',mnist_attributes,[44]) > 0
+    assert robustness_image_metric(create_linkage_hierarchy,
+                                   load_tcav_vectors_simple,'mnist',mnist_attributes,[43,44,45]) > 0
+    assert robustness_image_metric(create_linkage_hierarchy,
+                                   load_tcav_vectors_simple,'mnist',mnist_attributes,[43]) > 0
+    assert robustness_image_metric(create_linkage_hierarchy,
+                                   load_tcav_vectors_simple,'mnist',mnist_attributes,[44]) > 0
     
-    assert robustness_image_metric(create_linkage_hierarchy,load_label_vectors_simple,'mnist',mnist_attributes,[44,45]) == 0
+    assert robustness_image_metric(create_linkage_hierarchy,
+                                   load_label_vectors_simple,'mnist',mnist_attributes,[43,44,45]) == 0
     
 def test_image_responsiveness():
-    mnist_attributes = ['{}_color'.format(i) for i in range(5)] + ['{}_number'.format(i) for i in range(5)]
+    mnist_attributes = get_mnist_attributes()
     
-    assert responsiveness_image_metric(create_linkage_hierarchy,load_tcav_vectors_simple,'mnist',mnist_attributes,[43,44,45]) > 0
-    assert responsiveness_image_metric(create_linkage_hierarchy,load_tcav_vectors_simple,'mnist',mnist_attributes,[43]) > 0
-    assert responsiveness_image_metric(create_linkage_hierarchy,load_tcav_vectors_simple,'mnist',mnist_attributes,[44])>0
-    
-    assert responsiveness_image_metric(create_linkage_hierarchy,load_label_vectors_simple,'mnist',mnist_attributes,[44,45]) == 24
+    assert responsiveness_image_metric(create_linkage_hierarchy,
+                                       load_tcav_vectors_simple,'mnist',mnist_attributes,[43,44,45]) > 0
+    assert responsiveness_image_metric(create_linkage_hierarchy,
+                                       load_tcav_vectors_simple,'mnist',mnist_attributes,[43]) > 0
+    assert responsiveness_image_metric(create_linkage_hierarchy,
+                                       load_tcav_vectors_simple,'mnist',mnist_attributes,[44])>0
+    assert responsiveness_image_metric(create_linkage_hierarchy,
+                                       load_label_vectors_simple,'mnist',mnist_attributes,[43,44,45]) > 20
     
 if __name__ == "__main__":
     test_stability()
